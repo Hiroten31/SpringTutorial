7d4c27182e4fc339aa3a8c4adde0818a0324c4d5
Hiroten31 <hiroten31@gmail.com>
2023-03-21 20:13:46 +0100

    Done first tests with POSTMAN, as well as adding, deleting and putting new students. The exercise from video tutorial is done completely by myself :D (in a static way).

--- src/main/java/com/example/tutorial/student/StudentService.java
+++ src/main/java/com/example/tutorial/student/StudentService.java
@@ -1,11 +1,11 @@
 package com.example.tutorial.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -22,6 +22,29 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("Email taken!");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists){
+            throw new IllegalStateException("Student with id " + studentId + " does not exists!");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists){
+            throw new IllegalStateException("Student with id " + studentId + " does not exists!");
+        }
+        Student student = studentRepository.getReferenceById(studentId);
+        student.setName("Mango");
+        student.setEmail("Mango@gmail.com");
     }
 }
