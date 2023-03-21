ebcc6e0a8f6eb77966e8d666adb29aa1eeb12409
Hiroten31 <hiroten31@gmail.com>
2023-03-21 17:32:05 +0100

    Implementing posting a student to a database. Trying POSTMAN.

--- src/main/java/com/example/tutorial/student/StudentConfig.java
+++ src/main/java/com/example/tutorial/student/StudentConfig.java
@@ -13,8 +13,8 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository){
         return args -> {
-            Student banan = new Student(1L, "Banan", "banan@gmail.com", LocalDate.of(2022, 03, 31), 21);
-            Student ananas = new Student("Ananas", "ananas@gmail.com", LocalDate.of(2023, 01, 7), 21);
+            Student banan = new Student(1L, "Banan", "banan@gmail.com", LocalDate.of(2016, 03, 31));
+            Student ananas = new Student("Ananas", "ananas@gmail.com", LocalDate.of(2020, 01, 7));
             repository.saveAll(List.of(banan, ananas));
         };
     }
